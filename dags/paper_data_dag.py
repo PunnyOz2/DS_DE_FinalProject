4d7cfdcf4fba87d6c28cd17bedc8e9ec7a83f74a
Tawatwee Aiemarreerat <punnyoz1103@gmail.com>
2024-05-05 19:36:56 +0700

    change code

--- dags/paper_data_dag.py
+++ dags/paper_data_dag.py
@@ -29,11 +29,13 @@ def read_data_to_redis():
                 count = 0
                 Maxcount = len(data)
                 for item in data:
-                    r.set(f"paper:{item['eid']}:references",
-                          json.dumps(item['references']))
+                    for reference in item['references']:
+                        r.rpush(
+                            f"paper:{item['eid']}:references", json.dumps(reference))
                     r.sadd("papereids", item['eid'])
-                    r.set(f"paper:{item['eid']}:affiliations",
-                          json.dumps(item['affiliations']))
+                    for affiliation in item['affiliations']:
+                        r.rpush(
+                            f"paper:{item['eid']}:affiliations", json.dumps(affiliation))
                     count += 1
                     print(f"{count}/{Maxcount} done")
 
@@ -50,14 +52,18 @@ def print_random_paper():
     r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
     temp = r.srandmember("papereids")
     print(temp)
-    print(r.get(f"paper:{temp}:references")[0])
-    print(json.loads(r.get(f"paper:{temp}:references"))[0])
+    print(r.lrange(f"paper:{temp}:references", 0, -1))
+    print(r.lrange(f"paper:{temp}:affiliations", 0, -1))
 
 
 def download_data():
-    url = 'https://github.com/PunnyOz2/data_for_DE_project/raw/main/archive.zip'
-    urllib.request.urlretrieve(url, 'archive.zip')
-    with zipfile.ZipFile('archive.zip', 'r') as zip_ref:
+    url = 'https://github.com/phumipatc/CU_Submissions/raw/77f67105fe542af0a92272313e9da65394192eac/Data_Sci/final_project/data/given_data.zip'
+    urllib.request.urlretrieve(url, 'given_data.zip')
+    with zipfile.ZipFile('given_data.zip', 'r') as zip_ref:
+        zip_ref.extractall('data')
+    url = 'https://github.com/phumipatc/CU_Submissions/raw/77f67105fe542af0a92272313e9da65394192eac/Data_Sci/final_project/data/scrape_data.zip'
+    urllib.request.urlretrieve(url, 'scrape_data.zip')
+    with zipfile.ZipFile('scrape_data.zip', 'r') as zip_ref:
         zip_ref.extractall('data')
 
 
